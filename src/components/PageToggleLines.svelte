4af0c40409f821a7086133a3c0a678d77126cbdc
Omar jaff <omarjaff95@gmail.com>
2020-02-09 19:58:41 +0300

    fixed height of lines

--- src/components/PageToggleLines.svelte
+++ src/components/PageToggleLines.svelte
@@ -8,21 +8,26 @@
   }
   .active-class {
     background-color: #fe2f4d;
-    width: 4rem;
+    width: 2.5rem;
+  }
+  @media screen and (min-width: 640px) and (max-width: 1023px) {
+    .active-class {
+      width: 4rem;
+    }
   }
   @media screen and (min-width: 1024px) {
     .active-class {
-      width: 6rem;
+      width: 5.5rem;
     }
   }
 </style>
 
 <ul
   class="flex flex-col-reverse sm:flex-col transform sm:transform-none
-  -rotate-90 translate-y-12 translate-x-3">
-  <li class="bg-white w-8 sm:w-10 lg:w-12 active-class line-hight my-5" />
-  <li class="bg-white w-8 sm:w-10 lg:w-12 line-hight my-5" />
-  <li class="bg-white w-8 sm:w-10 lg:w-12 line-hight my-5" />
-  <li class="bg-white w-8 sm:w-10 lg:w-12 line-hight my-5" />
-  <li class="bg-white w-8 sm:w-10 lg:w-12 line-hight my-5" />
+  -rotate-90 translate-y-16 translate-x-3">
+  <li class="bg-white w-5 sm:w-10 xl:w-12 active-class line-hight my-5" />
+  <li class="bg-white w-5 sm:w-10 xl:w-12 line-hight my-5" />
+  <li class="bg-white w-5 sm:w-10 xl:w-12 line-hight my-5" />
+  <li class="bg-white w-5 sm:w-10 xl:w-12 line-hight my-5" />
+  <li class="bg-white w-5 sm:w-10 xl:w-12 line-hight my-5" />
 </ul>
