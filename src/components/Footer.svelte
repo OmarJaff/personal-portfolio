f4331371334cec5897dab558dd3c5a884ec55fdd
Omar jaff <omarjaff95@gmail.com>
2020-02-26 20:39:10 +0300

    finished footer design

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,36 +1,36 @@
 <script>
+  import ScoialIcons from '../components/SocialIcons.svelte'
 
+  let currentYear = new Date()
 </script>
 
 <style>
   .line-shadow {
     box-shadow: 0 32px 32px 0 rgba(0, 0, 0, 0.64);
   }
-  /* .line-style::before {
-    top: 0;
-    height: 1px;
-    max-width: 1726px;
-    width: calc(100% - 48px);
+  .bg-gradiant {
+    background: radial-gradient(
+      circle,
+      rgba(253, 47, 77, 1) 0%,
+      rgba(26, 28, 51, 1) 81%
+    );
   }
-  .line-style:after,
-  .line-style:before {
-    content: '';
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    display: block;
-  } */
+  .custom-height {
+    height: 1.5px;
+  }
 </style>
 
-<div class="line-shadow absolute w-screen flex items-center justify-center h-2">
-  <svg
-    width="927"
-    height="2"
-    viewBox="0 0 927 2"
-    class="w-1/2 fill-current z-40"
-    xmlns="http://www.w3.org/2000/svg">
-    <path d="M927 0H0L480.968 2L927 0Z" fill="#FE2F4D" />
-  </svg>
+<div
+  class="line-shadow absolute w-full flex items-center justify-center h-2
+  flex-wrap">
 
-  <div class="flex flex-col relative h-40 w-full" />
+  <span class=" bg-vived-red bg-gradiant custom-height w-11/12" />
+
+  <div class="flex flex-col relative h-40 w-full items-center justify-evenly">
+    <ScoialIcons IconSize="h-4 sm:h-5 xl:h-8" IconColor="text-vived-red" />
+    <span class=" text-form-input-gray text-xs sm:text-sm">
+      <span class="text-vived-red">&copy; {currentYear.getFullYear()}</span>
+      Omar Jaff - All rights reserved.
+    </span>
+  </div>
 </div>
