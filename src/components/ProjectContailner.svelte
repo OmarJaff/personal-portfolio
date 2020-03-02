fc923ae6338353667d110c75b1bd4e1ce6e5e749
Omar jaff <omarjaff95@gmail.com>
2020-03-02 18:27:32 +0300

    added animation on scrolling pages

--- src/components/ProjectContailner.svelte
+++ src/components/ProjectContailner.svelte
@@ -57,12 +57,14 @@
   items-center sm:items-start custom-shadow-all ">
   <div
     class=" flex pictuer-width lg:h-auto left-0 z-10 photo-shadow left-picture
-    h-custom w-full bg-dark-blue">
+    h-custom w-full bg-dark-blue"
+    data-aos="fade-right">
     <slot name="containerDesign" />
   </div>
   <div
     class=" text-width text-h-custom flex xl:justify-end xl:absolute top-custom
-    h-auto top-custom-2 font-mono ">
+    h-auto top-custom-2 font-mono "
+    data-aos="fade-left">
     <div class="custom-shadow h-full w-full flex flex-col justify-between ">
       {#if containerTitle}
         <h1
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte'
   import Portfolio from '../components/PortfolioStyle.svelte'
   import Title from '../components/Title.svelte'
   import MyFullName from '../components/MyFullName.svelte'
@@ -10,6 +11,15 @@
   import Projects from '../components/sections/Projects.svelte'
   import ContactForm from '../components/sections/ContactForm.svelte'
   import Footer from '../components/sections/Footer.svelte'
+  import AOS from 'aos'
+
+  onMount(() => {
+    AOS.init({
+      duration: 800,
+      disable: 'phone',
+      once: false,
+    })
+  })
 </script>
 
 <style>
@@ -21,9 +31,9 @@
 <svelte:head>
   <title>Omar Jaff</title>
 </svelte:head>
+
 <div id="home" class="flex flex-col justify-center h-screen w-full font-all ">
   <section class="flex h-full w-full my-48">
-
     <div class="flex w-full flex-col items-center ">
       <div class="text-white text-4xl">
         <Portfolio />
@@ -45,38 +55,37 @@
   <ScrollIcon />
   <ScrollDowmFlag />
 </div>
-<div class="flex flex-col h-screen justify-between w-full">
+<div class="flex flex-col h-screen justify-between w-full ">
   <div
     id="about"
-    class="flex justify-center sm:items-center mx-10 xl:mx-32 w-auto py-4 mt-20
-    lg:mt-48">
+    class="flex justify-center sm:items-center mx-10 xl:mx-32 w-auto py-4 my-10
+    sm:my-20">
     <About />
   </div>
 
   <div
     id="skills"
-    class="py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto mt-24
-    lg:mt-56">
+    class="py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto my-10
+    sm:my-20">
     <Skills />
   </div>
 
   <div
     id="projects"
-    class=" py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto mt-24
-    lg:mt-56">
+    class=" py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto my-10
+    sm:my-20">
     <Projects />
   </div>
 
   <div
     id="contact"
-    class="py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto mt-24
-    lg:mt-56">
+    class="py-4 flex justify-center sm:items-center mx-10 xl:mx-32 w-auto my-10
+    sm:my-20">
     <ContactForm />
   </div>
 
   <div
-    class="flex justify-center sm:items-center mx-10 xl:mx-32 w-auto mt-24
-    lg:mt-56 relative">
+    class="flex justify-center sm:items-center mx-10 xl:mx-32 w-aut relative">
     <Footer />
   </div>
 </div>
