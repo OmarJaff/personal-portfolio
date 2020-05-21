1d7d39fda322574325436df08d75f2301b33eb1e
Omar Jaff <omarjaff95@gmail.com>
2020-05-21 18:27:18 +0300

    updated packages

--- tailwind.js
+++ tailwind.js
@@ -1,30 +1,31 @@
 module.exports = {
+  purge: [],
   theme: {
     extend: {
       colors: {
-        'dark-blue': '#1A1C33',
-        'light-blue': '#061F3D',
-        'dark-blue-deep': '#12142A',
-        'vived-red': '#FE2F4D',
-        'vived-red-lighter': '#FF4D5A',
-        'form-input-gray': '#666877',
-        'text-white-lighter': '#E0E1EB',
+        "dark-blue": "#1A1C33",
+        "light-blue": "#061F3D",
+        "dark-blue-deep": "#12142A",
+        "vived-red": "#FE2F4D",
+        "vived-red-lighter": "#FF4D5A",
+        "form-input-gray": "#666877",
+        "text-white-lighter": "#E0E1EB",
       },
       fontFamily: {
         nixieOne: [
-          'Nixie One',
-          'Noto Sans',
-          'sans-serif',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
+          "Nixie One",
+          "Noto Sans",
+          "sans-serif",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
         ],
-        fenix: ['Fenix', 'serif'],
-        robotoMono: ['"Roboto Mono"', 'sans-serif', 'Arial'],
+        fenix: ["Fenix", "serif"],
+        robotoMono: ['"Roboto Mono"', "sans-serif", "Arial"],
       },
     },
   },
   variants: {},
   plugins: [],
-}
+};
