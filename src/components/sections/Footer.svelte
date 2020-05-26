3671d9b42e25449773be75e837531310ad140fcd
Omar Jaff <omarjaff95@gmail.com>
2020-05-26 14:16:55 +0300

    updated the color of the footer

--- src/components/sections/Footer.svelte
+++ src/components/sections/Footer.svelte
@@ -13,12 +13,11 @@
 <div class="absolute flex justify-center h-2 flex-wrap w-full ">
 
   <div
-    class="flex flex-col h-40 items-center justify-center bg-dark-blue-deep
-    w-screen ">
+    class="flex flex-col h-40 items-center justify-center bg-gray-800 w-screen ">
 
-    <ScoialIcons IconSize="h-4 sm:h-5 xl:h-8 my-3" IconColor="teal-600" />
+    <ScoialIcons IconSize="h-4 sm:h-5 xl:h-8 my-3" IconColor="teal-400" />
 
-    <span class=" text-color text-xs sm:text-sm">
+    <span class=" text-gray-500 text-xs sm:text-sm">
       <span>&copy; {currentYear.getFullYear()}</span>
       Developed and designed by Omar S. Jaff
     </span>
