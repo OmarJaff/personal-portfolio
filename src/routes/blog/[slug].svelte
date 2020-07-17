50dc36156bfcc2d206c5585f489544fa1d33adad
Omar Jaff <omarjaff95@gmail.com>
2020-07-17 19:05:42 +0300

    updated .yml file

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -27,48 +27,13 @@
   };
 </script>
 
-<style>
-  /*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
-  .content :global(h2) {
-    font-size: 1.4em;
-    font-weight: 500;
-  }
-
-  .content :global(pre) {
-    background-color: #f9f9f9;
-    box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
-    padding: 0.5em;
-    border-radius: 2px;
-    overflow-x: auto;
-  }
-
-  .content :global(pre) :global(code) {
-    background-color: transparent;
-    padding: 0;
-  }
-
-  .content :global(ul) {
-    line-height: 1.5;
-  }
-
-  .content :global(li) {
-    margin: 0 0 0.5em 0;
-  }
-</style>
-
 <svelte:head>
   <title>{post.title}</title>
 </svelte:head>
 
 <h1>{post.title}</h1>
-
+<p>{post.description}</p>
+<h1>{post.date}</h1>
 <img class="h-12 w-12" src={post.thumbnail} alt="image" />
 
 <div class="prose prose-2xl">
