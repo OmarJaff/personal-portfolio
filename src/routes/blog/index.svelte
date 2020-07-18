e474211ead0f2dddac5e06e1edf1276043b141d7
Omar Jaff <omarjaff95@gmail.com>
2020-07-18 16:25:51 +0300

    adding hover class to read more button

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -79,25 +79,28 @@
               <p class="text-base lg:text-lg text-gray-600">
                 {post.description}
               </p>
-              <div class="flex space-x-2 items-center">
-                <a href="blog/{post.slug}" class="text-teal-400 text-base">
+              <div class="flex ">
+                <a
+                  href="blog/{post.slug}"
+                  class="text-teal-400 text-base flex items-center
+                  hover:text-teal-500">
                   Read more
+                  <svg
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    class=" w-5 h-5 ml-2"
+                    stroke="currentColor">
+                    <path
+                      d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168
+                      5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477
+                      4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0
+                      3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5
+                      18c-1.746 0-3.332.477-4.5 1.253" />
+                  </svg>
                 </a>
-                <svg
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  class="text-teal-400 w-5 h-5"
-                  stroke="currentColor">
-                  <path
-                    d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477
-                    3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5
-                    1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477
-                    4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746
-                    0-3.332.477-4.5 1.253" />
-                </svg>
               </div>
 
             </div>
