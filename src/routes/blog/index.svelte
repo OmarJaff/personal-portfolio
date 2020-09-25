49b2dfc3eba36a7456ac83ca0428f96017278395
Omar Jaff <omarjaff95@gmail.com>
2020-09-26 00:45:18 +0300

    fixing the orientation problem

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -14,6 +14,7 @@
   import Header from "../../components/Header.svelte";
   const arabic = /[\u0600-\u06FF]/;
   let readmore = "Read more";
+
   if (arabic.test(posts.title)) {
     readmore = "انقر للمزيد";
   }
