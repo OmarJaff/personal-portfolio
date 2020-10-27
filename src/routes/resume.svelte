7d8b547cfe1ea4571a55111294dace1b076c8b63
OmarJaff <me@omarjaff.com>
2020-10-27 22:37:26 +0300

    updated now

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,6 +1,18 @@
 <script>
   import Header from "../components/Header.svelte";
   import ResumeTemplate from "../components/Resume/ResumeTemplate.svelte";
+
+  async function file_get_contents(uri, callback) {
+    let res = await fetch(uri),
+      ret = await res.text();
+    return callback ? callback(ret) : ret; // a Promise() actually.
+  }
+
+  //   file_get_contents("https://omarjaff.com/callback", console.log);
+  // or
+  file_get_contents("https://omarjaff.com/callback").then(ret =>
+    console.log(ret)
+  );
 </script>
 
 <div id="resume" class="block absolute w-full justify-center inset-0 h-20">
