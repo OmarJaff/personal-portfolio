2e3340062939195661e74c426552f3bec8c72318
OmarJaff <me@omarjaff.com>
2020-10-27 22:31:44 +0300

    updated

---
+++ src/routes/callback.svelte
@@ -0,0 +1,15 @@
+<script>
+  async function file_get_contents(uri, callback) {
+    let res = await fetch(uri),
+      ret = await res.text();
+    return callback ? callback(ret) : ret; // a Promise() actually.
+  }
+
+  //   file_get_contents("https://omarjaff.com/callback", console.log);
+  // or
+  file_get_contents("https://omarjaff.com/callback").then(ret =>
+    console.log(ret)
+  );
+</script>
+
+<div class="content" />
